101c62745638bc7ab16eaba12e31b896ce235a9d
Patrick Richardson <Patrick Richardson>
2017-04-09 23:28:29 -0700

    Updated project with Omar's implementation of the GUI

---
+++ app/src/main/java/com/bitjunkie/smartdialer/Tab2Log.java
@@ -0,0 +1,22 @@
+package com.bitjunkie.smartdialer;
+
+/**
+ * Created by Omar on 4/4/2017.
+ */
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Log extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2log, container, false);
+        return rootView;
+    }
+}
+
